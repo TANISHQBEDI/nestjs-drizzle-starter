5fbce9a76f40e0ad9e4d2681215d1257b5c30c5d
Tanishq Bedi <66412510+TANISHQBEDI@users.noreply.github.com>
2025-12-05 19:06:11 +1100

    Nest new app init with config setup

---
+++ src/config/env.ts
@@ -0,0 +1,12 @@
+export default () => ({
+  env: process.env.NODE_ENV || 'production',
+  port: parseInt(process.env.PORT!) || 3000,
+  database: {
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT!),
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    name: process.env.DATABASE_NAME,
+    url: process.env.DATABASE_URL
+  }
+})
